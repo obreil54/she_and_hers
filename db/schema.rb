cdb9f8f30b318b99b2261ad316cd3c6d96aa847c
Ilya Obretetskiy <obreil54@gmail.com>
2024-09-19 14:52:20 +0100

    Added adress fields to the users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_19_085219) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_19_135028) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_19_085219) do
     t.string "last_name"
     t.string "phone"
     t.boolean "admin", default: false
+    t.string "address_line1"
+    t.string "address_line2"
+    t.string "city"
+    t.string "state"
+    t.string "postal_code"
+    t.string "country"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
