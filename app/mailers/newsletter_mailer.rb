ce7ce42abc2bac240da528bf4b844397b8937a24
Ilya Obretetskiy <obreil54@gmail.com>
2024-11-06 17:25:49 +0000

    Adding further logs

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -4,7 +4,8 @@ class NewsletterMailer < ApplicationMailer
   def thank_you_email(email, discount_code)
     @discount_code = discount_code
     mail(to: email, subject: 'Thank you for signing up! Enjoy 10% off your next purchase.')
-    Rails.logger.info "Mailer 'from' domain: #{mail.from.first.split('@').last}"
-    Rails.logger.info "Mailing to: #{@user.email}"
+    Rails.logger.info "Mailchimp Transactional SMTP Response: #{ActionMailer::Base.smtp_settings[:address]}"
+    Rails.logger.error "Email error: #{e.message}"
+    Rails.logger.error e.backtrace.join("\n")
   end
 end
